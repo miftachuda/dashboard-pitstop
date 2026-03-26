2f6546e953ddf37af96acbfd111e10dca3044888
Miftachul Huda <miftachulcc@gmail.com>
2026-03-26 15:48:11 +0700

    change domain

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,6 +1,6 @@
 // /lib/pocketbase.ts
 import PocketBase from "pocketbase";
 //console.log("PocketBase instance created");
-const pb = new PocketBase("https://base.loc-2.com");
+const pb = new PocketBase("https://base.miftachuda.my.id");
 pb.autoCancellation(false);
 export { pb };
